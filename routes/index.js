8362e3e55c984804c478dfd746a1721cb1d05ef4
Klint Tiley <kctiley@gmail.com>
2015-10-11 10:31:29 -0600

    Sessions working.

--- routes/index.js
+++ routes/index.js
@@ -3,14 +3,14 @@ var router = express.Router();
 var db = require('monk')('localhost/authentication-2');
 var Users = db.get('users');
 
-/* GET home page. */
+
 router.get('/', function(req, res, next) {
   Users.find({}, function(err, record){
     res.render('index', { title: 'Authentication-2', allUsers: record });
   });
 });
 
-/* GET home page. */
+
 router.get('/signup', function(req, res, next) {
   res.render('sign', { title: 'Authentication-2', subtitle: 'Sign Up' });
 });
@@ -22,16 +22,14 @@ router.get('/signin', function(req, res, next) {
 
 router.post('/', function(req, res, next){
   console.log("Req.body.inputEmail" + req.body.inputEmail);
-  console.log("COOKIES>>>>>>>" + req.cookies.cookieNAME)
-  res.cookie("cookieNAME", req.body.inputEmail)
-
+  req.session.username = req.body.inputEmail;
   Users.insert({email: req.body.inputEmail, password: req.body.password}, function(err, record){
     res.render('index', {title: 'Authentication-2', statusSignedIn: true, userEmail: req.body.inputEmail})
   })
 })
 
 router.get('/logout', function(req, res, next){
-  res.clearCookie("name");
+  req.session = null;
   res.redirect('/')
 })
 
