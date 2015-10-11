35907ad4161e7af4281bf70eafb45439b78e1c01
Klint Tiley <kctiley@gmail.com>
2015-10-11 14:22:25 -0600

    Signin and signup validations working

--- routes/index.js
+++ routes/index.js
@@ -10,23 +10,73 @@ router.get('/', function(req, res, next) {
   });
 });
 
-
 router.get('/signup', function(req, res, next) {
   res.render('sign', { title: 'Authentication-2', subtitle: 'Sign Up' });
 });
 
+router.post('/signup', function(req, res, next){
+
+  var errors = [];
+  if (!req.body.inputEmail){
+    errors.push("No email entered")
+  }
+  if (!req.body.inputPassword){
+    errors.push("No password entered")
+  }
+  if (errors.length){
+    res.render('sign', {title: "Auth-2", errors: errors}) 
+  }
+  else{
+    Users.find({email: req.body.inputEmail}, function(err, record){
+      if (record.length){
+        errors.push('Useremail not available');
+      }
+      if (errors.length){
+        res.render('sign', { title: 'Authentication-2', subtitle: 'Sign Up', errors: errors})
+      }
+      else{
+        req.session.username = req.body.inputEmail;
+        Users.insert({email: req.body.inputEmail, password: req.body.inputPassword}, function(err, record){
+          res.render('index', {title: 'Authentication-2', statusSignedIn: true, userEmail: req.body.inputEmail})
+        })
+      };
+    })
+  }
+})
 
 router.get('/signin', function(req, res, next) {
-  res.render('sign', { title: 'Authentication-2', subtitle: 'Sign In', user: 'user' });
+    res.render('sign', { title: 'Authentication-2', subtitle: 'Sign In', user: 'user' });
 });
 
-router.post('/', function(req, res, next){
-  console.log("Req.body.inputEmail" + req.body.inputEmail);
-  req.session.username = req.body.inputEmail;
-  Users.insert({email: req.body.inputEmail, password: req.body.password}, function(err, record){
-    res.render('index', {title: 'Authentication-2', statusSignedIn: true, userEmail: req.body.inputEmail})
-  })
-})
+router.post('/signin', function(req, res, next){
+  var errors = [];
+  if (!req.body.inputEmail){
+    errors.push("No email entered")
+  }
+  if (!req.body.inputPassword){
+    errors.push("No password entered")
+  }
+  if (errors.length){
+    res.render('sign', {title: "Auth-2", errors: errors}) 
+  }
+  else{
+    Users.findOne({email: req.body.inputEmail}, function(err, record){ 
+      if(record){
+        console.log('The record.....' + record.email);
+        if(record.password === req.body.inputPassword){
+          req.session.username = req.body.inputEmail;;
+          res.render('index', {title: "Auth-2",statusSignedIn: true, userEmail: req.body.inputEmail})
+        }
+      }
+      else{
+        errors.push('Cannot find user email')
+        res.render('sign', {title: "Auth-2", user: 'user', errors: errors})      
+      }
+    })
+  }
+})  
+
+
 
 router.get('/logout', function(req, res, next){
   req.session = null;
